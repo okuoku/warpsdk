8235284f35e04db956e80aa72bae6ce55aa2628f
okuoku <mjt@cltn.org>
2025-02-26 23:35:36 +0900

    Add protopaths

---
+++ protopaths.cmake
@@ -0,0 +1,9 @@
+set(WARPSDK_ROOT e:/yunibuild/llvm/bin)
+
+
+set(WARPSDK_AR ${WARPSDK_ROOT}/llvm-ar.exe)
+set(WARPSDK_CLANG ${WARPSDK_ROOT}/clang.exe)
+set(WARPSDK_LINKER ${WARPSDK_ROOT}/wasm-ld.exe)
+
+
+
