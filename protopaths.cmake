bc29099a9a990425fd5b0fcd14e8ecf15ee6dc43
okuoku <mjt@cltn.org>
2025-07-27 18:11:35 +0900

    protopaths: Detect tools from `PATH`

--- protopaths.cmake
+++ protopaths.cmake
@@ -1,12 +1,6 @@
-set(WARPSDK_ROOT e:/yunibuild/llvm/bin)
-
-
-set(WARPSDK_AR ${WARPSDK_ROOT}/llvm-ar.exe)
-set(WARPSDK_CLANG ${WARPSDK_ROOT}/clang.exe)
-set(WARPSDK_LINKER ${WARPSDK_ROOT}/wasm-ld.exe)
+find_program(WARPSDK_AR llvm-ar)
+find_program(WARPSDK_CLANG clang)
+find_program(WARPSDK_LINKER wasm-ld)
 
 set(WARPSDK_SYSROOT ${CMAKE_CURRENT_LIST_DIR}/sysroot)
 set(WARPSDK_POSIXROOT ${CMAKE_CURRENT_LIST_DIR}/yuniposix)
-
-
-
