0fc74764ae345e54e9ee7157c3b429cd91c268d2
okuoku <mjt@cltn.org>
2025-09-11 18:16:01 +0900

    crt: Implement main2 and main3

--- crt/warpcrt_exec.c
+++ crt/warpcrt_exec.c
@@ -1,14 +1,19 @@
-//int main(int ac, char** av, char** envp);
-int main(int ac, char** av);
+int __main_argc_argv(int ac, char** av);
 void __call_exitprocs(int, void*);
 void __libc_init_array(void);
+
+int __attribute__((weak))
+main(int ac, char** av, char** envp){
+    (void) envp;
+    return __main_argc_argv(ac, av);
+}
 
 int __attribute__((export_name ("__start")))
 __start(void){
     int r;
     __libc_init_array();
 
-    r = main(0, 0);
+    r = main(0, 0, 0);
 
     __call_exitprocs(r, 0);
 
