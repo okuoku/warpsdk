8fd169ac1b7997d6182a396ea308caa4a3b70b00
okuoku <mjt@cltn.org>
2026-01-26 22:08:25 +0900

    build-llvm: Strip output

--- lib_pkgpickup.cmake
+++ lib_pkgpickup.cmake
@@ -16,6 +16,7 @@ function(runpickup prefix)
         list(APPEND bins "${pth}")
     endforeach()
 
+    # Pass1: Detect symlink and follow targets
     set(bins0 "${bins}")
     foreach(e IN LISTS bins0)
         if(IS_SYMLINK "${e}")
@@ -27,8 +28,38 @@ function(runpickup prefix)
 
     list(REMOVE_DUPLICATES bins)
 
-    # Copy files (Use COPY here; COPY_FILE cannot copy symlink)
-    file(COPY ${bins} DESTINATION ${pkgdest}/bin
+    # Pass2: Split list into EXEC/Symlink
+    set(execs)
+    set(symlinks)
+    foreach(e IN LISTS bins)
+        if(IS_SYMLINK "${e}")
+            list(APPEND symlinks "${e}")
+        else()
+            list(APPEND execs "${e}")
+        endif()
+    endforeach()
+
+    # Strip executables FIXME: Save debug info somewhere
+    foreach(e IN LISTS execs)
+        if(WIN32)
+            # MSVC already uses split-debuginfo
+            file(COPY ${e} DESTINATION ${pkgdest}/bin
+                USE_SOURCE_PERMISSIONS)
+        else()
+            cmake_path(GET e FILENAME fn)
+            cmake_path(SET out NORMALIZE ${pkgdest}/bin/${fn})
+            file(MAKE_DIRECTORY ${pkgdest}/bin)
+            message(STATUS "Strip ${e} => ${out}")
+            execute_process(COMMAND
+                strip -o ${out} ${e}
+                RESULT_VARIABLE rr)
+            if(rr)
+                message(FATAL_ERROR "Failed to strip ${e} (${rr})")
+            endif()
+        endif()
+    endforeach()
+    # Copy symlinks (Use COPY here; COPY_FILE cannot copy symlink)
+    file(COPY ${symlinks} DESTINATION ${pkgdest}/bin
         USE_SOURCE_PERMISSIONS)
 
     foreach(d IN LISTS pkgdirs)
